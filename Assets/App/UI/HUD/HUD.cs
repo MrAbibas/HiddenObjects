667882276ae6107575059fea76e98fbea234a2c7
USOD-Dev <prowodniktoka@gmail.com>
2025-11-26 22:14:10 +0200

    update hud and create item configs

--- Assets/App/UI/HUD/HUD.cs
+++ Assets/App/UI/HUD/HUD.cs
@@ -1,16 +1,36 @@
+using System.Collections.Generic;
+using App.Gameplay.Items;
+using TMPro;
 using UnityEngine;
+using VContainer;
 
-public class HUD : MonoBehaviour
+namespace App.UI.HUD
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class HUD : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private RectTransform targetItemsContainer;
+        [SerializeField] private TargetItemOnUI targetItemOnUIPrefab;
+        [SerializeField] private TMP_Text lvlText;
+        [SerializeField] private TMP_Text timerText;
+        [SerializeField] private string timeFormat;
+        private Dictionary<ItemType, TargetItemOnUI> _targetItems;
+        private ItemConfigs _itemConfigs;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Inject]
+        public void Construct(ItemConfigs itemConfigs)
+        {
+            _itemConfigs = itemConfigs;
+        }
+
+        public void Init(Dictionary<ItemType, int> targetItems)
+        {
+            _targetItems = new Dictionary<ItemType, TargetItemOnUI>();
+            foreach (var targetItem in targetItems)
+            {
+                var itemOnUI = Instantiate(targetItemOnUIPrefab, targetItemsContainer);
+                itemOnUI.Show(_itemConfigs.Configs[targetItem.Key].Sprite, targetItem.Value);
+                _targetItems.Add(targetItem.Key, itemOnUI);
+            }
+        }
     }
 }
