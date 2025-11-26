23ff98b5307d2d3a220fdfcc54ee818a6dd601d0
USOD-Dev <prowodniktoka@gmail.com>
2025-11-26 22:16:29 +0200

    create item configs asset and register

--- Assets/App/LifetimeScopes/GameplayLifetimeScope.cs
+++ Assets/App/LifetimeScopes/GameplayLifetimeScope.cs
@@ -1,3 +1,5 @@
+using App.Gameplay.Items;
+using UnityEngine;
 using VContainer;
 using VContainer.Unity;
 
@@ -5,8 +7,11 @@ namespace App.LifetimeScopes
 {
     public class GameplayLifetimeScope : LifetimeScope
     {
+        [SerializeField] private ItemConfigs itemConfigs;
+        
         protected override void Configure(IContainerBuilder builder)
         {
+            builder.RegisterInstance(itemConfigs);
         }
     }
 }
