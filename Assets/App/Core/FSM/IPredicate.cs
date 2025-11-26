28f26aa8f5d2984cf68c43fe39bff9b97b7c7e85
USOD-Dev <prowodniktoka@gmail.com>
2025-11-26 17:06:10 +0200

    import FSM core

---
+++ Assets/App/Core/FSM/IPredicate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace App.Core.FSM
+{
+    public interface IPredicate
+    {
+        bool Evaluate();
+    }
+
+    public class FuncPredicate : IPredicate
+    {
+        private readonly Func<bool> func;
+
+        public FuncPredicate(Func<bool> func)
+        {
+            this.func = func;
+        }
+        
+        public bool Evaluate() => func.Invoke();
+    }
+}
---
+++ Assets/App/Core/FSM/IState.cs
@@ -0,0 +1,9 @@
+﻿namespace App.Core.FSM
+{
+    public interface IState
+    {
+        void Enter();
+        void Update();
+        void Exit();
+    }
+}
