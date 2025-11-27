5cb93b95dbaca3073c33119540bdd8491c38403e
USOD-Dev <prowodniktoka@gmail.com>
2025-11-28 01:58:38 +0200

    create timer

---
+++ Assets/App/Gameplay/Common/Timer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace App.Gameplay.Common
+{
+    public class Timer
+    {
+        public event Action<float> OnTimeUpdated;
+        public float Duration { get; private set; }
+        public float ElapsedTime { get; private set; }
+        public float TimeLeft => Duration - ElapsedTime;
+
+        public Timer(float duration)
+        {
+            Duration = duration;
+            ElapsedTime = 0;
+        }
+    }
+}
