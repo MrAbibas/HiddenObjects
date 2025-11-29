f7d6fdc60d93b824da2da88c88d02b7e7b59df36
USOD-Dev <prowodniktoka@gmail.com>
2025-11-29 23:10:17 +0200

    add level result checker to states

--- Assets/App/Gameplay/GameplayFSM/States/InitLevelGameplayState.cs
+++ Assets/App/Gameplay/GameplayFSM/States/InitLevelGameplayState.cs
@@ -8,18 +8,24 @@ namespace App.Gameplay.GameplayFSM.States
         private readonly Timer _lvlTimer;
         private readonly LevelConfig _levelConfig;
         private readonly GameplayStateMachine _stateMachine;
+        private readonly ILevelResultChecker _levelResultChecker;
         
-        public InitLevelGameplayState(Timer lvlTimer, LevelConfig levelConfig,  GameplayStateMachine stateMachine)
+        public InitLevelGameplayState(Timer lvlTimer,
+            LevelConfig levelConfig,
+            GameplayStateMachine stateMachine,
+            ILevelResultChecker levelResultChecker)
         {
             _lvlTimer = lvlTimer;
             _levelConfig = levelConfig;
             _stateMachine = stateMachine;
+            _levelResultChecker = levelResultChecker;
         }
 
         public void Enter()
         {
             _lvlTimer.Start(_levelConfig.Time);
             _stateMachine.LvlInitialized = true;
+            _levelResultChecker.Initialize();
         }
 
         public void Update()
